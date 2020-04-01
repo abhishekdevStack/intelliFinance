26a48295e2a3a5a97270e5381313a15ddcb5f799
abhishekdevStack <abhishek.srm@outlook.com>
2020-04-02 03:01:09 +0530

    Updates

---
+++ precache-manifest.b16c93c4d58028fec7c007e3f8cd42f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55fa3954eea45c8857abca7a34f1d91b",
+    "url": "/intelliFinance/index.html"
+  },
+  {
+    "revision": "ec975dac5602beafd886",
+    "url": "/intelliFinance/static/css/main.304c2e2d.chunk.css"
+  },
+  {
+    "revision": "ca3c4a62b05ef94bc107",
+    "url": "/intelliFinance/static/js/2.72fe0fd4.chunk.js"
+  },
+  {
+    "revision": "e6fc1b149346e824e2d6d62d0056499b",
+    "url": "/intelliFinance/static/js/2.72fe0fd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec975dac5602beafd886",
+    "url": "/intelliFinance/static/js/main.d86bc124.chunk.js"
+  },
+  {
+    "revision": "5119c3dfd218cecc934c",
+    "url": "/intelliFinance/static/js/runtime-main.136374e6.js"
+  }
+]);
